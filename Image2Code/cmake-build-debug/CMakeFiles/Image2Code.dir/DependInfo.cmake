d4a074850c0bd5586c2a54f4822325369c988bfd
Ibrahim Radwan <i.radwan1996@gmail.com>
2017-12-18 13:17:11 +0200

    Add preprocessing

---
+++ Image2Code/cmake-build-debug/CMakeFiles/Image2Code.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/ibrahimradwan/Desktop/lines/Image2Code/Image2Code/Main.cpp" "/Users/ibrahimradwan/Desktop/lines/Image2Code/Image2Code/cmake-build-debug/CMakeFiles/Image2Code.dir/Main.cpp.o"
+  "/Users/ibrahimradwan/Desktop/lines/Image2Code/Image2Code/Segmentation/CharSegmentation.cpp" "/Users/ibrahimradwan/Desktop/lines/Image2Code/Image2Code/cmake-build-debug/CMakeFiles/Image2Code.dir/Segmentation/CharSegmentation.cpp.o"
+  "/Users/ibrahimradwan/Desktop/lines/Image2Code/Image2Code/Segmentation/LineSegmentation.cpp" "/Users/ibrahimradwan/Desktop/lines/Image2Code/Image2Code/cmake-build-debug/CMakeFiles/Image2Code.dir/Segmentation/LineSegmentation.cpp.o"
+  "/Users/ibrahimradwan/Desktop/lines/Image2Code/Image2Code/Segmentation/Segmentation.cpp" "/Users/ibrahimradwan/Desktop/lines/Image2Code/Image2Code/cmake-build-debug/CMakeFiles/Image2Code.dir/Segmentation/Segmentation.cpp.o"
+  "/Users/ibrahimradwan/Desktop/lines/Image2Code/Image2Code/Segmentation/WordSegmentation.cpp" "/Users/ibrahimradwan/Desktop/lines/Image2Code/Image2Code/cmake-build-debug/CMakeFiles/Image2Code.dir/Segmentation/WordSegmentation.cpp.o"
+  "/Users/ibrahimradwan/Desktop/lines/Image2Code/Image2Code/Utilities/Utilities.cpp" "/Users/ibrahimradwan/Desktop/lines/Image2Code/Image2Code/cmake-build-debug/CMakeFiles/Image2Code.dir/Utilities/Utilities.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "/usr/local/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Image2Code/cmake-build-debug/CMakeFiles/Image2Code.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Image2Code.dir/Segmentation/CharSegmentation.cpp.o"
+  "CMakeFiles/Image2Code.dir/Segmentation/LineSegmentation.cpp.o"
+  "CMakeFiles/Image2Code.dir/Segmentation/Segmentation.cpp.o"
+  "CMakeFiles/Image2Code.dir/Segmentation/WordSegmentation.cpp.o"
+  "CMakeFiles/Image2Code.dir/Utilities/Utilities.cpp.o"
+  "CMakeFiles/Image2Code.dir/Main.cpp.o"
+  "Image2Code.pdb"
+  "Image2Code"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Image2Code.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
